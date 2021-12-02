5637637cb7bf9e136ede8b38331ad00c59781342
Nelson Lara <nelsonlarajr@gmail.com>
2021-12-02 00:48:48 -0300

    Incluído Função e Tela de Autenticação

--- core/views.py
+++ core/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout # precisa para efetuar o login
+from django.contrib import messages
 
 # Create your views here.
 # Função para redirecionar a raiz, precisa acrescenatr redirect em from django.shortcuts import render, redirect
 #def index(request):
     #return redirect('/agenda/')
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou Senha Inválido!!!") # precisou from django.contrib import messages
+    return redirect('/')
+
+
+@login_required(login_url='/login/') # solicitar login precisou importar from django.contrib.auth.decorators import login_required
 
 def lista_eventos(request):
     usuario = request.user                         ### Receber o usuario logado no Django
