941f68bb0bf4e4a00a2059b6974589801be4b16d
Nelson <nelsonlarajr@gmail.com>
2021-12-15 16:13:18 -0300

    removir configuracao do postgresql agenda/settings/heroku.py

--- agenda/settings/heroku.py
+++ agenda/settings/heroku.py
@@ -6,10 +6,4 @@ env = environ.Env()
 
 DEBUG = env.bool("DEBUG", False)
 
-#SECRET_KEY = env("SECRET_KEY")
-
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
-
-#DATABASES = {
-#    "default": env.db(),
-#}
